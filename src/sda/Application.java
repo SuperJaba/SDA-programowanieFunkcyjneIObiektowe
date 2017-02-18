6791efa9c1c52f0a76690f9b714988cda173c12a
unknown <duneval@gmail.com>
2017-02-18 14:35:52 +0100

    Test Hello World for git

---
+++ src/sda/Application.java
@@ -0,0 +1,10 @@
+package sda;
+
+/**
+ * Created by RENT on 2017-02-18.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
